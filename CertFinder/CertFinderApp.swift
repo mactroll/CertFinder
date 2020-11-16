4fcf405b91c7b72d27a7f94599590bad4cbc37c2
Joel Rennich <joel@orchardandgrove.com>
2020-11-16 11:04:19 -0600

    Initial Commit

---
+++ CertFinder/CertFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CertFinderApp.swift
+//  CertFinder
+//
+//  Created by jcadmin on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct CertFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
